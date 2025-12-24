f0f2841b4d3c4b00fc0893af18386170235eaf01
Kodak678 <95473076+Kodak678@users.noreply.github.com>
2025-12-24 23:47:15 +0000

    Added the settings button and now everything works

--- app/src/main/java/Scrambler/ScramblerMainActivity.java
+++ app/src/main/java/Scrambler/ScramblerMainActivity.java
@@ -1,6 +1,9 @@
 package Scrambler;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,15 @@ public class ScramblerMainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button openSettingsButton = findViewById(R.id.open_settings_button);
+        openSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScramblerMainActivity.this, rkr.simplekeyboard.inputmethod.latin.settings.SettingsActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
